e3676d626cf880ab46ac3d7be8f4a3d819913a44
hyochan <overcome26@gmail.com>
2023-03-15 23:59:21 +0900

    그래들 변경

--- app/src/main/java/com/github/henryJung/app/model/MockEntity.kt
+++ app/src/main/java/com/github/henryJung/app/model/MockEntity.kt
@@ -1,11 +1,13 @@
 package com.github.henryJung.app.model
 
+import com.github.henryJung.annotation.Mapper
+import com.github.henryJung.annotation.MapperProperty
 import com.github.henryJung.app.TestModel
 
-//@Mapper(TestModel::class)
+@Mapper(TestModel::class)
 data class MockEntity(
     val id: Long,
-//    @MapperProperty("name")
+    @MapperProperty("name")
     val name222 : String,
     val dasd:String
 )
